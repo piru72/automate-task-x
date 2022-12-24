3244db9ce063a0cc04dc4c316215c683078dd1fa
piru72 <parvezdirom2000@gmail.com>
2022-12-25 03:28:35 +0600

    routine of cse dept splitted

--- AUST_ROUTINE_PROVIDER/routine_splitter.rb
+++ AUST_ROUTINE_PROVIDER/routine_splitter.rb
@@ -1,14 +1,83 @@
-require 'combine_pdf'
+# required gems for the operations
+require "combine_pdf"
+require "pdf-reader"
 
-pages = CombinePDF.load("Input/CSE_Class_Routine_Spring2022.pdf").pages;
+# Function to read every text of every routine and generate a file name for each routine in this (Year_4_Semester_2_SectionB) format
+def file_name_creator(pdf,department)
 
-i = 0
-pages.each do |page|
-   pdf = CombinePDF.new
-   pdf << page
-   pdf.save("#{i}.pdf")
-   i+=1
-   if i == 2
-    break
-   end
+  # Array to store the title of the routines
+  routines = Array.new()
+
+  i = 0
+
+  # iterating through the pages of the pdf
+  pdf.pages.each do |page|
+
+    #extracting the texts of the pages
+    writings = page.text
+
+    # Constructing a string of a certain range from the texts of the pages
+    unprocessed_title = ""
+    for j in 600..705
+      unprocessed_title += writings[j]
+    end
+
+    # Removing unwanted characters and adding a format to the texts
+    processed_title = unprocessed_title.delete(" ").chomp.gsub("\n", "").gsub(",", "_").gsub(":", "_").gsub("(", "_").gsub(")", "")
+
+    # Removing any unwanted character from the processed string
+    final_title = processed_title.slice(0, processed_title.length - (processed_title.length - 26))
+    final_title += "_#{department}"
+
+    # adding the title to the routine
+    routines << final_title
+    i += 1
+
+    # Break condition for safety as last page is usually empty
+    if i + 1 == pdf.pages.size
+      break
+    end
+  end
+
+  # returning the list of titles
+  return routines
 end
+
+# function to save the page that has the desired routine
+def file_saver(pages, routines, wanted_routine)
+  i = 0
+
+  # iterating through the pages of the pdf
+  pages.each do |page|
+    pdf = CombinePDF.new
+    pdf << page
+
+    # If the courrent page matches with the desired routine then save the routine and break the loop
+    if (routines[i] == wanted_routine)
+      pdf.save("#{routines[i]}.pdf")
+      break
+    end
+
+    i += 1
+  end
+end
+
+# The main objective of this program is to find the routine of the given semester
+def main()
+
+  # Prepations to start finding the file
+  intput_file_name = "Input/CSE_Class_Routine_Spring2022.pdf"
+  pages = CombinePDF.load(intput_file_name).pages
+  pdf = PDF::Reader.new(intput_file_name)
+
+  # Creates a list of all the routines accordingly
+  routines = file_name_creator(pdf,"CSE")
+  # adding an empty page
+  routines << "LAST PAGE"
+  wanted_routine = "Year_3_Semester_1_SectionC_CSE"
+
+  # saving the wanted routine
+  file_saver(pages, routines, wanted_routine)
+end
+
+main()
