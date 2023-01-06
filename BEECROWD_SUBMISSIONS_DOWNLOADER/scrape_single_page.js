4c5498001d3d7d232a5235b72cf51b38eac39982
piru72 <parvezdirom2000@gmail.com>
2023-01-06 15:50:08 +0600

    executed for extracting executive details

--- BEECROWD_SUBMISSIONS_DOWNLOADER/scrape_single_page.js
+++ BEECROWD_SUBMISSIONS_DOWNLOADER/scrape_single_page.js
@@ -6,25 +6,30 @@ const writeStream = fs.createWriteStream('single_page_notice.csv');
 // Write headers to csv file
 writeStream.write(`Text,Link,Date\n`);
 
-request('https://www.aust.edu/notice', (error, response, html) => {
+request('https://www.beecrowd.com.br/judge/en/runs', (error, response, html) => {
 
     if (!error && response.statusCode == 200) {
         console.log('Successfully scraped the website.');
         const $ = cheerio.load(html);
-        $('.notice_slider_container').each((i, el) => {
-            const Text = $(el).find('.notice_text').children('a').next().text();
-            const Link = $(el).find('.notice_text').children('a').attr('href');
-            const month = $(el).find('.notice_date').find('.month').text();
-            const day = $(el).find('.notice_date').find('.day').text();
-            const year = $(el).find('.notice_date').find('.year').text();
-            const Date = month + ' ' + day + ' ' + year;
 
-            console.log(Text);
-            console.log(Link);
-            console.log(Date);
-            console.log('\n');
-            writeStream.write(`${Text},${Link},${Date}\n`);
-        });
+        const pageHeaderHeading = $('.filter-box');
+
+        console.log(pageHeaderHeading.find('h3').text());
+
+        // $('.impar').each((i, el) => {
+        //     const Text = $(el).find('.tiny').children('a').attr('href');
+        //     // const Link = $(el).find('.notice_text').children('a').attr('href');
+        //     // const month = $(el).find('.notice_date').find('.month').text();
+        //     // const day = $(el).find('.notice_date').find('.day').text();
+        //     // const year = $(el).find('.notice_date').find('.year').text();
+        //     // const Date = month + ' ' + day + ' ' + year;
+
+        //     console.log(Text);
+        //     // console.log(Link);
+        //     // console.log(Date);
+        //     // console.log('\n');
+        //     // writeStream.write(`${Text},${Link},${Date}\n`);
+        // });
         console.log('Scraping Complete');
     }
 });
---
+++ FILE_NAME_EXTRACTOR/main.py
@@ -0,0 +1,17 @@
+import os
+
+# folder path
+dir_path = r'G:\\executives\\'
+
+# list to store files
+res = []
+
+# Iterate directory
+for path in os.listdir(dir_path):
+    # check if current path is a file
+    if os.path.isfile(os.path.join(dir_path, path)):
+        res.append(path)
+for details in (res):
+    print (details)
+
+
