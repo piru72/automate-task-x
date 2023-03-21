cf05a250577720f15059818a1773283484e190e2
piru72 <parvezdirom2000@gmail.com>
2023-03-21 06:57:10 +0600

    fix (json) : fromat created

--- SCRAPPER_YOU_TUBE_PLAYLIST/script.js
+++ SCRAPPER_YOU_TUBE_PLAYLIST/script.js
@@ -1,17 +1,23 @@
-var els = document.getElementsByClassName('yt-simple-endpoint style-scope ytd-playlist-video-renderer');
-var thumbnails = document.getElementsByClassName('yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded');
-var id="";
-var title="";
+var els = document.getElementsByClassName(
+  "yt-simple-endpoint style-scope ytd-playlist-video-renderer"
+);
+var thumbnails = document.getElementsByClassName(
+  "yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded"
+);
+var videoId = "";
+var title = "";
 var thumbnail = "";
-for(i = 0;i<els.length;i++){
-    var el = els[i];
-    var t = thumbnails[i];
-    
-	id += (el.href.split('?v=')[1].split('&list')[0] + "\n");
-    title += (el.title + "\n");
-    thumbnail += (t.src + "\n");
+var playlistData = {};
+for (i = 0; i < 5; i++) {
+  var el = els[i];
+  var t = thumbnails[i];
 
+  videoId = el.href.split("?v=")[1].split("&list")[0] ;
+
+  playlistData[videoId] = {
+    videoId: videoId,
+    title: el.title,
+    thumbnail: t.src,
+  };
 }
-console.log(id);
-console.log(title);
-console.log(thumbnail);
+console.log(playlistData);
